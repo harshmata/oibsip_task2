7c985e00127d110cd5da87e66775c2181bf5a545
Harsh Mata <harshmata3@gmail.com>
2023-05-05 15:43:43 +0530

    Task 2 added

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView stopwatchTextView;
+    private Button startButton, pauseButton, resetButton;
+    private boolean running;
+    private int seconds = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        stopwatchTextView = findViewById(R.id.stopwatchTextView);
+        startButton = findViewById(R.id.startButton);
+        pauseButton = findViewById(R.id.pauseButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseTimer();
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+    }
+
+    private void startTimer() {
+        running = true;
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%02d:%02d:%02d", hours, minutes, secs);
+                stopwatchTextView.setText(time);
+                if (running) {
+                    seconds++;
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        });
+    }
+
+    private void pauseTimer() {
+        running = false;
+    }
+
+    private void resetTimer() {
+        running = false;
+        seconds = 0;
+        stopwatchTextView.setText("00:00:00");
+    }
+}
